249314304a858014b9c0cb366f82a63dce370ca7
RobzLegz <robzlegz@gmail.com>
2022-02-15 09:55:51 +0200

    get ai rating images

---
+++ get_images.py
@@ -0,0 +1,47 @@
+sources = [
+    "https://images.generated.photos/yZe4-qr0QKM1djPOhY9TfynsPSNdAWX7TzDpiXDFWas/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MjU0ODE0LmpwZw.jpg",
+    "https://images.generated.photos/lkn7ax5_eDpiTrF-rQaDysMtcUifIGzORjJHsff1er0/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NDU4OTk4LmpwZw.jpg",
+    "https://images.generated.photos/lk2lz33KJK5YwL47MbCBNSeWx2sFpkdxV7OXZNook7g/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NjY4NTAyLmpwZw.jpg",
+    "https://images.generated.photos/Ra3atuRPvZSe0FkVXmykFEl-oiLNEuc_U1rTkZ3gZs8/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MzI3MTAxLmpwZw.jpg",
+    "https://images.generated.photos/EgeIsZICtvZYSfGDSvxudoIpmofEeQjO3SCkgO-mnqg/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTA2MTg4LmpwZw.jpg",
+    "https://images.generated.photos/VYL7d4Nq4XsKzE8eBUqIYFPlstgr8u8ByQ7u8pGlmT4/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MzM3MzM2LmpwZw.jpg",
+    "https://images.generated.photos/CKV1B10pTGTC8kTmRD90jlI8s7gxNH7rKjYn7Ki7ag0/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MzAyNzM5LmpwZw.jpg",
+    "https://images.generated.photos/6U3iNISSc530D4G2xd1ROPhnjhxZuQw8q024o4gXz0w/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODkyMjU4LmpwZw.jpg",
+    "https://images.generated.photos/0kTfTK1NMd1kaMxX7LD8lou4M22jWq0SWn2je8sSn8E/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTc3MTcyLmpwZw.jpg",
+    "https://images.generated.photos/iHHRDEcoJU4rc99tvOFzVu5GLZ3EJzXFXXioP4juyYQ/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/Mzc1NTcyLmpwZw.jpg",
+    "https://images.generated.photos/c9F03tZOv9C3V75Gfk6rHd6_bhYju59JKzjUs0wF9TI/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDIxMDc4LmpwZw.jpg",
+    "https://images.generated.photos/9ZEMVNyp9Ui1PGJxowA8yAVBYnYN3roo3ihKTn7-1Zw/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTc3MDkzLmpwZw.jpg",
+    "https://images.generated.photos/e-8myNuiq7A4VxqsaALhHaORjv_Cbg81ixJuk0_arxc/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDUyNzUwLmpwZw.jpg",
+    "https://images.generated.photos/KV41ExKj_W-OE4CJ2ynIUhNLuw6iKhqCqq5493PjDCQ/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTM4NTMwLmpwZw.jpg",
+    "https://images.generated.photos/6e3mt20h0XYdwq8BWYTdlDNN7QytAX9BdwVbn9nERh4/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDcwMTQ0LmpwZw.jpg",
+    "https://images.generated.photos/nNri8FQUmZEL-NZ7wJ-lb4nTeWEYrKBYusZDxAO332I/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDE1NzI0LmpwZw.jpg",
+    "https://images.generated.photos/iCCRIKw5GrVW1iAgwcdafHhgrcaTtJ2DWCuqnEjl5AU/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTY2NDY1LmpwZw.jpg",
+    "https://images.generated.photos/kZLoFbBdUwj_VHxqkswL-4ihi6WStwMxWKiMGyFCqT4/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDU5MzI0LmpwZw.jpg",
+    "https://images.generated.photos/znhyRY9h44uh4mB-sXtpfWYqgCbkMwZXo1M1CnHC2VI/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTYwNDQ1LmpwZw.jpg",
+    "https://images.generated.photos/KKDa_QVdKLiNbgeQZBfRDfX0tpYI85m1L-7oJrl37J0/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDAzMzA0LmpwZw.jpg",
+    "https://images.generated.photos/KapPAbWIs61cyZJCI4j-F3zTypQGoM5xN_6WrelfH-I/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTMzODM1LmpwZw.jpg",
+    "https://images.generated.photos/zXOQldf8AhGnwkHDlMqBisOpreeMk_sLmw-FmT75CE0/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTYxNTMwLmpwZw.jpg",
+    "https://images.generated.photos/5fCcKYvgwpPLV5iPWYDDni1TwoPizvR_9FPon71rK68/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTUxMjk0LmpwZw.jpg",
+    "https://images.generated.photos/0f-ePwJMlHeTQwCGm5SXP4uSEQdXuPdgSZw8H8wCA0c/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzgwMDI3LmpwZw.jpg",
+    "https://images.generated.photos/ok-i-WQZ7_CETyeTXSCnswMoA2Nwd0Aa8k5Rg52dPMA/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODc5OTQ4LmpwZw.jpg",
+    "https://images.generated.photos/vDNq_haRIrunxUjVy4oojokd4lVX-IcjiPZz_UQn328/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODI4OTczLmpwZw.jpg",
+    "https://images.generated.photos/wsA6ycpsiZCVRFi9RAUm2jol0DSB7KuBaSjjyAT7pUA/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NDkzNTMzLmpwZw.jpg",
+    "https://images.generated.photos/lsEOhoaSWbWz5-MdPneh1fHzRKnJvFEbsxRmdk8j5dg/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTYzNDk2LmpwZw.jpg",
+    "https://images.generated.photos/zLKiCg_-L4ioBIk4FScL1qRIped8OTB64CmuuE5Il7Y/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MjIzMzI2LmpwZw.jpg",
+    "https://images.generated.photos/MFvbNmtDg_d2WWbquPsmH6bnrlBTNgqws5c6NX8Mrrg/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTcyODc4LmpwZw.jpg",
+]
+
+f = open("scores.txt", "a")
+
+for source in sources:
+    formated_source = source.replace("https://images.generated.photos/", "")
+    formated_source = formated_source.replace(".jpg", "")
+
+    source_id = formated_source.split("/")[5]
+    starting_score = 1400
+
+    write_data = f"{source_id}|{starting_score}|{source}\n"
+
+    f.write(write_data)
+
+f.close()
