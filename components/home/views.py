27235bb6845d86b6cb2c12f6f31b35bdfc4b95e0
RobzLegz <robzlegz@gmail.com>
2022-02-15 14:38:06 +0200

    scrapefile

--- components/home/views.py
+++ components/home/views.py
@@ -3,8 +3,15 @@ from flask.helpers import url_for
 
 home = Blueprint("home", __name__)
 
+scores = open("ratepic/scores.txt", "r")
+print(scores.read())
+
+image1 = ""
+image2 = ""
 
 @home.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
-        
+    return render_template("index.html", image1=image1, image2=image2)
+
+def click_image():
+    pass
